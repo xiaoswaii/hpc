03aa60289eca367668728cd44a36a7f4069092cc
xiaoswaii <b05106028@ntu.edu.tw>
2019-10-17 14:58:34 +0800

    to top

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 
 $(document).ready(function() {
     // Add smooth scrolling on all links inside the navbar
-    $("#infox, #introx, #timetablex, #contactx").on('click', function(event) {
+    $("#infox, #introx, #timetablex, #contactx, #placex").on('click', function(event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
@@ -40,3 +40,42 @@ $(document).ready(function() {
         } // End if
     });
 });
+
+function initMap() {
+    var lct = {
+        lat: 25.0208863,
+        lng: 121.5398093
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: lct
+    });
+    var marker = new google.maps.Marker({
+        position: lct,
+        map: map
+    });
+}
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+var body = $("html, body");
+  body.stop().animate({scrollTop:0}, 500, 'swing', function() { 
+ });
+}
+
+
